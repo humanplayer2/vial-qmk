e9eb29d5f87cc7dd7a512d7997faf75259feafe8
humanplayer2 <rendsvig@gmail.com>
2026-02-06 15:57:54 +0100

    vial unlock pins

--- keyboards/humanplayer2/underset/keymaps/vial/config.h
+++ keyboards/humanplayer2/underset/keymaps/vial/config.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xE3, 0x5C, 0x0E, 0x26, 0x79, 0x90, 0xBC, 0xBB}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
